e1a00c3f90725cef4d5b2b43d53da305edc32171
Fabrizio <fabri.rebecca@gmail.com>
2012-04-03 13:26:37 +0200

    Updated

--- esercizi.Plasm.Simplexn.2.js
+++ esercizi.Plasm.Simplexn.2.js
@@ -118,9 +118,74 @@ var drawSolidDome = function(rInf,rSup,width,color){
 
 	var mapped = MAP(mapping)(domain);
 
-	DRAW(mapped);
+	//DRAW(mapped);
 	COLOR(color)(mapped);
+	return mapped;
 };
+
+
+//CILINDRO PIENO
+var cilindroPieno = function(r,h,n,m,p,color) {
+	r = r || 1;
+	h = h || 1;
+
+	n = n || r*30;
+
+	m = m || 1;
+	p = p || 1;
+	color = color || [0,1,0];
+
+	var dominio = SIMPLEX_GRID([REPEAT(n)(2*PI/n), REPEAT(m)((r)/m), REPEAT(p)(h/p) ]);
+	
+	var mapping = function(pt) {
+		return [
+			pt[1]*SIN(pt[0]),
+			pt[1]*COS(pt[0]),
+		 	pt[2]
+		];
+	};
+
+	var mapped = MAP(mapping)(dominio);
+	return mapped;
+
+	//DRAW(mapped);
+	//COLOR(color)(mapped);
+
+};
+
+//BASE DI CILINDRI SFERICA
+var drawGrigliaSfericaDiCilindri = function(r,numCil,h){
+	var cilindro = cilindroPieno(1,h);
+	var angolo = (2*PI)/numCil;
+
+
+	return STRUCT(
+		REPLICA(numCil)
+		([cilindroPieno(r,h),T([0,1])([r*COS(angolo),r*SIN(angolo)])])
+		);
+};
+
+var drawGrigliaSfericaDiCilindri2 = function(r,numCil,h){
+	r = r || 11;
+	numCil = numCil || 8;
+	h = h || 20;
+	var cilindro = cilindroPieno(1,h);
+	var angolo = (2 * PI)/numCil;
+	var columns = [];
+for (var i = 0; i < numCil; i++) {
+	var trasl = i * angolo;
+	columns.push(T([0,1]) ([r*COS(trasl), r*SIN(trasl)]) (cilindro));}
+var colonnato = STRUCT(columns);
+DRAW(colonnato);
+};
+
+var drawPantheon = function(){
+var dome = drawSolidDome(10,3,2,[1,0,0]);
+var finalDome = T([2])([20])(dome);
+DRAW(finalDome);
+drawGrigliaSfericaDiCilindri2();
+
+}
 
 
 
